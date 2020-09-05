303fd8d701ca917ec07654e70de84797e569eba9
Atakan Salar <a.atakansalar@gmail.com>
2020-09-06 02:03:13 +0300

    Modified box_title to modify histogram summary output

--- scratch.py
+++ scratch.py
@@ -1,27 +1,18 @@
 # scratch.py
 from bashplotlib.scatterplot import plot_scatter
+from bashplotlib.histogram import plot_hist
 
 x_coords = [-10,20,30]
 y_coords = [-10,20,30]
 width = 10
 char = 'x'
-color = 'pink'
+color = 'yellow'
 title = 'My Test GraphGraphGraphGraph'
 align = 'center'
 x_axis = 'x axis placeholder'
 y_axis = 'y axis placeholder'
 
-plot_scatter(
-    None,
-    x_coords,
-    y_coords,
-    width,
-    char,
-    color,
-    title,
-    x_axis,
-    y_axis,
-    align,
-    True
-)
+plot_scatter(None, x_coords, y_coords, width, char, color, title, x_axis, y_axis, align, True)
 
+
+plot_hist("exp.txt", showSummary=True)
